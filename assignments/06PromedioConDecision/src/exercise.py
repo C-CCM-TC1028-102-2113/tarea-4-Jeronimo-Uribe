354ae8c2a46b48d927e98b0d3becece1c275bf88
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-09-07 18:16:53 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
+    print('Introduce la calificación: ')
+    calificación=float(input(''))
+    total=0
+    n=0
+    while calificación>0:
+        total=total+calificación
+        n=n+1
+        calificación=float(input(''))
+    if calificación<0:
+        promedio=total/n
+        print(promedio)
+        
     pass
 if __name__=='__main__':
     main()
