ed6ac5583188547b1ff0dcf3fa2219bb4cd409bb
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-09-07 19:13:23 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
+    n=int(input('Dame un número entero: '))
+    num=1
+    while num<n:
+        if num%2==0:
+            print(str(num)+('-%'))
+            num=num+1
+        elif num%2!=0:
+            print(str(num)+('-#'))
+            num=num+1
+    if num==n:
+        if num%2==0:
+            print(str(num)+('-%'))
+        elif num%2!=0:
+            print(str(num)+('-#')) 
+    
     pass
 
 if __name__=='__main__':   
